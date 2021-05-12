b98de1d03001f5018bcb36458d8598221ae678a2
gianna <giannacrisha@gmail.com>
2021-05-13 05:55:28 +0800

    periodic table update

---
+++ periodictable.js
@@ -0,0 +1,8 @@
+function appearLanthanide() {
+  var x = document.getElementById("lanthanide");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
